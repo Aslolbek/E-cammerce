e72e8c638b9c0505e935db5ffc0787759f06f643
Your Name <youremail@domain.com>
2025-02-16 20:47:11 +0500

    guard removed

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -1,26 +1,27 @@
 /* eslint-disable prettier/prettier */
-/* eslint-disable prettier/prettier */
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+// /* eslint-disable prettier/prettier */
+// /* eslint-disable prettier/prettier */
+// import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+// import { JwtService } from '@nestjs/jwt';
 
-@Injectable()
-export class JwtAuthGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+// @Injectable()
+// export class JwtAuthGuard implements CanActivate {
+//   constructor(private readonly jwtService: JwtService) {}
 
-  canActivate(context: ExecutionContext): boolean {
-    const request = context.switchToHttp().getRequest();
-    const authHeader = request.headers.authorization;
+//   canActivate(context: ExecutionContext): boolean {
+//     const request = context.switchToHttp().getRequest();
+//     const authHeader = request.headers.authorization;
 
-    if (!authHeader) {
-      throw new UnauthorizedException('Token yo‘q!');
-    }
+//     if (!authHeader) {
+//       throw new UnauthorizedException('Token yo‘q!');
+//     }
 
-    try {
-      const token = authHeader.split(' ')[1];
-      request.user = this.jwtService.verify(token); // ✅ Tokenni tekshiramiz
-      return true;
-    } catch (error) {
-      throw new UnauthorizedException('Noto‘g‘ri token!');
-    }
-  }
-}
+//     try {
+//       const token = authHeader.split(' ')[1];
+//       request.user = this.jwtService.verify(token); // ✅ Tokenni tekshiramiz
+//       return true;
+//     } catch (error) {
+//       throw new UnauthorizedException('Noto‘g‘ri token!');
+//     }
+//   }
+// }
